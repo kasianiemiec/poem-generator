b07a8cecbbfada99e561d348fbbd6723b365d680
kasianiemiec <kasia.a.niemiec@gmail.com>
2025-03-06 16:03:52 +0200

    implemented API integration

--- src/app.js
+++ src/app.js
@@ -1,13 +1,25 @@
-function handleSubmit(event) {
-  event.preventDefault();
-  alert("Generating a poem for you!");
-
+function displayResponse(response) {
   new Typewriter("#poem", {
-    strings: "Gentle buds emerge",
+    strings: response.data.answer,
     autoStart: true,
     cursor: null,
     delay: 40,
   });
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let subject = document.querySelector("#user-word");
+  let apiKey = "98bc9f158331o422ta5db05dbd90ba36";
+  let context =
+    "You are a romantic heiku expert and every time you are being asked, you create a unique heiku in the subject of the provided word and display it making sure each line is seperated with a <br />";
+  let prompt = `Please generate a heiku about ${subject.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayResponse);
+
+  let poem = document.querySelector("poem");
+  poem.innerHTML = "Generating a heiku for you...";
 }
 
 let poemFormElemnt = document.querySelector("#poem-generator-form");
